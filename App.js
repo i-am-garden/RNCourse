a9ec04d59c61615dd1a8e20764a6612f3fe03ffe
i-am-garden <tigerkjw11@gmail.com>
2022-08-28 14:12:44 +0900

    21강 KJW

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder='Your course goal!' />
         <Button style={{flex: 1}} title ='Add Goal'/>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text> HeyHey!!!List of goals!!! </Text>
       </View>
     </View>
@@ -16,19 +16,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     paddingTop : 50,
     paddingHorizontal: 16,
     flexDirection: 'column',
     justifyContent: 'space-between'
   },
   inputContainer: {
-    flexDirection: 'row'
+    flex : 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#ffffff',
+    borderColor: '#cccccc',
     width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5
   }
 });
